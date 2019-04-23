54bb054867a4f50fbf1a10fc5d74a91de1c6026a
Luis Miguel Alvarez <lalvarez@switch.local>
2019-04-23 09:31:31 -0300

    first commit de TestNG Project

---
+++ src/tests/FirstTests.java
@@ -0,0 +1,14 @@
+package tests;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class FirstTests {
+	
+	@Test
+	public void miPrimerTest() {
+		Assert.assertTrue(1==2);
+		
+	}
+	
+}
