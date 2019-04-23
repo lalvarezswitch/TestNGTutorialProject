7eaba10a77585139e8dec8eb0dd1fc90e75f633d
Luis Miguel Alvarez <lalvarez@switch.local>
2019-04-23 10:06:05 -0300

    Commit de TestNG Project

--- src/tests/FirstTests.java
+++ src/tests/FirstTests.java
@@ -7,8 +7,26 @@ public class FirstTests {
 	
 	@Test
 	public void miPrimerTest() {
-		Assert.assertTrue(1==2);
-		
+		Assert.assertTrue(1==1);
+		Assert.assertTrue('A'=='A');
 	}
 	
+	@Test
+	public void miSegundoTest() {
+		int i = 1;
+		int j = 2;
+		Assert.assertFalse(i==j);
+	}
+	
+	@Test
+	public void miTercerTest() {
+		Assert.assertEquals(5, 5);
+		Assert.assertEquals('C', 'C');
+	}
+	
+	@Test
+	public void miCuartoTest() {
+		Assert.assertNotEquals(4, 8);
+		Assert.assertNotEquals('4', 'H');
+	}
 }
